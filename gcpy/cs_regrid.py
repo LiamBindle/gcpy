4d4087499277150cd56246d7a0b4d84be177b038
Liam Bindle <liambindle@gmail.com>
2020-04-16 17:54:06 -0600

    added progress bar

--- gcpy/cs_regrid.py
+++ gcpy/cs_regrid.py
@@ -1,6 +1,7 @@
 import argparse
 import hashlib
 import os.path
+from tqdm import tqdm
 
 import numpy as np
 import xarray as xr
@@ -113,7 +114,7 @@ if __name__ == '__main__':
     )
 
     oface_datasets = []
-    for oface in range(6):
+    for oface in tqdm(range(6)):
         oface_regridded = [regridder(ds_in.isel(face=iface), keep_attrs=True) for iface, regridder in regridders[oface].items()]
         oface_regridded = xr.concat(oface_regridded, dim='intersecting_ifaces').sum('intersecting_ifaces')
         oface_datasets.append(oface_regridded)
